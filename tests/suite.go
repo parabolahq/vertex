a7c85409fa2432f8ce6658f54c8fc4a59434974c
Ivan <23701564+IvanProgramming@users.noreply.github.com>
2022-08-01 22:03:41 +0300

    Added websocket connection tests

--- tests/suite.go
+++ tests/suite.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"github.com/stretchr/testify/suite"
 	"gopkg.in/olahol/melody.v1"
 	"vertex/config"
@@ -12,9 +13,11 @@ type VertexTestSuite struct {
 	suite.Suite
 	Gin    *gin.Engine
 	Melody *melody.Melody
+	Dialer *websocket.Dialer
 }
 
 func (s *VertexTestSuite) SetupTest() {
 	s.Gin, s.Melody = routing.SetupRoutes()
 	config.LoadConfigs()
+	s.Dialer = websocket.DefaultDialer
 }
---
+++ tests/websockets.go
@@ -0,0 +1,35 @@
+package tests
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"net/http/httptest"
+	"strings"
+	"vertex/communication"
+)
+
+func (s *VertexTestSuite) TestWebsocketDisconnection() {
+	testServer := httptest.NewServer(s.Gin)
+	defer testServer.Close()
+	conn, _, err := s.Dialer.Dial(
+		fmt.Sprintf("%s/ws", strings.Replace(testServer.URL, "http", "ws", 1)),
+		nil,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	err = conn.WriteJSON(communication.UserRequest{
+		ServiceAlias: "vertex",
+		MethodName:   "disconnect",
+		Params:       gin.H{},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = conn.ReadJSON(nil)
+	// If connection is closed, err on read will be occurred
+	assert.ErrorContains(s.T(), err, "close")
+}
